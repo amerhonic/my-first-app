fb1bc26605dc763757d7c7bb98fb2abee7d55fa7
Amer <amer2014np@gmail.com>
2023-04-08 14:12:36 +0200

    flex

---
+++ src/compoments/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Navbar.css"
+import {Navbar} from "./components/Navbar/Navbar"
+function Navbar() {
+    return (
+       <header className="header">
+        
+       </header> 
+    )
+}
+export default App
